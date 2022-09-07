b0b9a791a304175a7a3a7b1d7b315e07021923ac
thomaslarner <tomoliverlarner@gmail.com>
2022-09-07 08:12:08 +0100

    various changes to full width charts

--- assets/javascript/cards.js
+++ assets/javascript/cards.js
@@ -4,3 +4,10 @@ fetch('/economics-column/cards.html')
 .then(data=>{
     econ.innerHTML=data
 })
+
+const blog = document.querySelector('.blog_cards')
+fetch('/blog/cards.html')
+.then(res=>res.text())
+.then(data=>{
+    blog.innerHTML=data
+})
