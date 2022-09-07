84dbf88642fd2b79509802dbfe85a68b57f5a08c
thomaslarner <tomoliverlarner@gmail.com>
2022-09-07 20:53:40 +0100

    filters in articles page + old articles in home

--- assets/javascript/cards.js
+++ assets/javascript/cards.js
@@ -11,3 +11,17 @@ fetch('/blog/cards.html')
 .then(data=>{
     blog.innerHTML=data
 })
+
+const all_articles = document.querySelector('.all_articles')
+fetch('/articles/cards.html')
+.then(res=>res.text())
+.then(data=>{
+    all_articles.innerHTML=data
+})
+
+const old_articles = document.querySelector('.old_articles')
+fetch('/articles/old-cards.html')
+.then(res=>res.text())
+.then(data=>{
+    old_articles.innerHTML=data
+})
