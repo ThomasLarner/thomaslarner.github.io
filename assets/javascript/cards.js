f304531583e4ea62a3c6b4faac3a6c7c8a463693
thomaslarner <tomoliverlarner@gmail.com>
2022-12-15 19:57:13 +0000

    fixed homepage old articles

--- assets/javascript/cards.js
+++ assets/javascript/cards.js
@@ -5,20 +5,6 @@ fetch('/economics-column/cards.html')
     econ.innerHTML=data
 })
 
-const blog = document.querySelector('.blog_cards')
-fetch('/blog/cards.html')
-.then(res=>res.text())
-.then(data=>{
-    blog.innerHTML=data
-})
-
-const all_articles = document.querySelector('.all_articles')
-fetch('/articles/cards.html')
-.then(res=>res.text())
-.then(data=>{
-    all_articles.innerHTML=data
-})
-
 const old_articles = document.querySelector('.old_articles')
 fetch('/articles/old-cards.html')
 .then(res=>res.text())
