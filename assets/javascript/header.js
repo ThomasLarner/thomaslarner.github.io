aed729e73fa9e56c4d968a9d07270b2f8706346c
thomaslarner <tomoliverlarner@gmail.com>
2023-03-08 21:24:07 +0000

    added close icon to menu toggle button

--- assets/javascript/header.js
+++ assets/javascript/header.js
@@ -1,14 +1,22 @@
 function menuFunction() {
     var close = document.getElementById("close");
     var moreText = document.getElementById("menu");
-    var btnText = document.getElementById("menuBtn");
+    var menu_open_btn = document.getElementById("menu_open_btn")
+    var menu_close_btn = document.getElementById("menu_close_btn")
 
     if (close.style.display === "none") {
         close.style.display = "inline";
         moreText.style.display = "none";
+        
+        menu_close_btn.style.display = "none";
+        menu_open_btn.style.display = "inline";
+
     } else {
         close.style.display = "none";
         moreText.style.display = "inline";
+
+        menu_close_btn.style.display = "inline";
+        menu_open_btn.style.display = "none";
     }
 }
 
