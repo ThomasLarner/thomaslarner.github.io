3e0653dc73f7726909b800720cad8b862c26056a
thomaslarner <tomoliverlarner@gmail.com>
2023-03-08 22:06:48 +0000

    added search result count to search page

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -2,10 +2,16 @@ function navbar_search_function() {
   var input = document.getElementById("navbar_search_bar");
   var filter = input.value.toLowerCase();
   var nodes = document.getElementsByClassName('card');
+  var result_count = document.getElementById("returned_search_results_count")
+  let count = 0;
+  
 
   for (i = 0; i < nodes.length; i++) {
     if (nodes[i].innerText.toLowerCase().includes(filter)) {
       nodes[i].style.display = "block";
+      count ++;
+      document.getElementById("numbResults").innerHTML = count/2;
+      result_count.style.display = "inline";
     } else {
       nodes[i].style.display = "none";
     }
