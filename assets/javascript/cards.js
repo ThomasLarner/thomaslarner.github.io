7a16f17644f477f5e4be5cc8ec63d5535c5d42c2
thomaslarner <tomoliverlarner@gmail.com>
2023-03-16 17:56:19 +0000

    fixed articles_db 404 error

--- assets/javascript/cards.js
+++ assets/javascript/cards.js
@@ -1,11 +1,11 @@
 const article_database = document.querySelector('.article_database')
-fetch('/article_db.html')
+fetch('/articles_db.html')
 .then(res=>res.text())
 .then(data=>{
     article_database.innerHTML=data
 })
 const article_database2 = document.querySelector('.article_database2')
-fetch('/article_db.html')
+fetch('/articles_db.html')
 .then(res=>res.text())
 .then(data=>{
     article_database2.innerHTML=data
