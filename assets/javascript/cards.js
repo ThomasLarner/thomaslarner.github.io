6d9812850be53e127f18861feee0a1d732b2ceb3
thomaslarner <tomoliverlarner@gmail.com>
2023-03-16 16:41:40 +0000

    removed old content and relocated article_db

--- assets/javascript/cards.js
+++ assets/javascript/cards.js
@@ -1,11 +1,11 @@
 const article_database = document.querySelector('.article_database')
-fetch('/database/article_database.html')
+fetch('/article_db.html')
 .then(res=>res.text())
 .then(data=>{
     article_database.innerHTML=data
 })
 const article_database2 = document.querySelector('.article_database2')
-fetch('/database/article_database.html')
+fetch('/article_db.html')
 .then(res=>res.text())
 .then(data=>{
     article_database2.innerHTML=data
